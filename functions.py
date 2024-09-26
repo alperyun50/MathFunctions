0dd97f37af262ca431ea7a485dc46ae96c5dc98d
alperyun50 <yavuzun50@hotmail.com>
2024-09-27 00:54:22 +0300

    functions added 2

--- functions.py
+++ functions.py
@@ -7,3 +7,17 @@ x_values = [0, 1, 2, 3]
 for x in x_values:
     y = f(x)
     print(y)
+
+
+# Charting a linear function in Python using SymPy
+from sympy import *
+
+x = symbols("x")
+f = 2*x + 1
+plot(f)
+
+
+# Charting an exponential function
+x = symbols("x")
+f = x**2 + 1
+plot(f)
