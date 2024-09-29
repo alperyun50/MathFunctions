a7b8a46bcfe8c7cfff79ea281321ebc8bf1f1503
alperyun50 <yavuzun50@hotmail.com>
2024-09-29 23:41:17 +0300

    function added 3

--- functions.py
+++ functions.py
@@ -11,6 +11,7 @@ for x in x_values:
 
 # Charting a linear function in Python using SymPy
 from sympy import *
+from sympy.plotting import plot3d
 
 x = symbols("x")
 f = 2*x + 1
@@ -21,3 +22,10 @@ plot(f)
 x = symbols("x")
 f = x**2 + 1
 plot(f)
+
+
+# f(x,y) = 2x + 3y
+x, y = symbols("x,y")
+f = 2*x + 3*y
+plot3d(f)
+
